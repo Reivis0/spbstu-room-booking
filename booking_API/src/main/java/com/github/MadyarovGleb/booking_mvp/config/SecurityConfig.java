9dd22f8cdb798689631b3b2c501121bcfc96b581
Kladkovoi Maksim <cladkovoj@yandex.ru>
2026-01-08 18:45:41 +0300

    Fix deploy

--- booking_API/src/main/java/com/github/MadyarovGleb/booking_mvp/config/SecurityConfig.java
+++ booking_API/src/main/java/com/github/MadyarovGleb/booking_mvp/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/refresh").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
